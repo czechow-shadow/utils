7037f010940bec65cf0d1d66b07ce3b44b551ec5
czechow <pawel.czechowicz@vp.pl>
2019-10-01 17:03:32 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+DEST_DIR=~/bin
+
+FILES=(mk-cabal-skel mk-nix-skel)
+FORCE=
+
+if [ $# -gt 1 ]; then
+  usage;
+  exit 1;
+fi
+
+if [ $# = 1 ]; then
+  DEST_DIR="$1";
+fi
+
+
+for i in ${FILES[@]}; do
+  f="$DEST_DIR/$i";
+  fh=`pwd`/$i;
+
+  if [ -e "$f" ]; then
+  	echo "$f already exists";
+	if [ -n "$FORCE" ]; then
+  	  echo "Removing $f";
+	else
+  	  echo "Aborted";
+	  exit 2;
+	fi
+  fi
+
+  ( cd $DEST_DIR && ln -s "$fh" "$i" )
+done
